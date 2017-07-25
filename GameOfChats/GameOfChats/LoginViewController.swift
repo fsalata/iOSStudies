1870be3ffdb0511738ddc3f5faf88541168ee5de
Fábio Salata <fabio.salata@dartdigital.com.br>
2017-07-25 09:41:53 -0300

    Layout tela

--- GameOfChats/GameOfChats/LoginViewController.swift
+++ GameOfChats/GameOfChats/LoginViewController.swift
@@ -27,6 +27,57 @@ class LoginViewController: UIViewController {
         button.setTitle("Login", for: UIControlState.normal)
         return button
     }()
+    
+    let nameTextField: UITextField = {
+        let textfield = UITextField()
+        textfield.translatesAutoresizingMaskIntoConstraints = false
+        textfield.placeholder = "Name"
+        return textfield
+    }()
+    
+    let nameSeparatorView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = UIColor(r: 220, g: 220, b: 220)
+        return view
+    }()
+    
+    let emailTextField: UITextField = {
+        let textfield = UITextField()
+        textfield.translatesAutoresizingMaskIntoConstraints = false
+        textfield.placeholder = "Email"
+        return textfield
+    }()
+    
+    let emailSeparatorView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = UIColor(r: 220, g: 220, b: 220)
+        return view
+    }()
+    
+    let passwordTextField: UITextField = {
+        let textfield = UITextField()
+        textfield.isSecureTextEntry = true
+        textfield.translatesAutoresizingMaskIntoConstraints = false
+        textfield.placeholder = "Password"
+        return textfield
+    }()
+    
+    let passwordSeparatorView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = UIColor(r: 220, g: 220, b: 220)
+        return view
+    }()
+    
+    let profileImageView: UIImageView = {
+        let imageview = UIImageView()
+        imageview.translatesAutoresizingMaskIntoConstraints = false
+        imageview.image = UIImage(named: "stark-wolf")
+        imageview.contentMode = UIViewContentMode.scaleAspectFit
+        return imageview
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,10 +86,19 @@ class LoginViewController: UIViewController {
         
         view.addSubview(containerView)
         view.addSubview(loginRegisterButton)
+        view.addSubview(profileImageView)
         
         setupContainerView()
         setupLoginButton()
+        setupProfileImageView()
         
+    }
+    
+    func setupProfileImageView() {
+        profileImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        profileImageView.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor).isActive = true
+        profileImageView.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        profileImageView.bottomAnchor.constraint(equalTo: containerView.topAnchor, constant: 12).isActive = true
     }
     
     func setupContainerView() {
@@ -47,6 +107,43 @@ class LoginViewController: UIViewController {
         containerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         containerView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
         containerView.heightAnchor.constraint(equalToConstant: 150).isActive = true
+        
+        containerView.addSubview(nameTextField)
+        nameTextField.topAnchor.constraint(equalTo: containerView.topAnchor).isActive = true
+        nameTextField.leftAnchor.constraint(equalTo: containerView.leftAnchor, constant: 12).isActive = true
+        nameTextField.widthAnchor.constraint(equalTo: containerView.widthAnchor).isActive = true
+        nameTextField.heightAnchor.constraint(equalTo: containerView.heightAnchor, multiplier: 1/3).isActive = true
+        
+        containerView.addSubview(nameSeparatorView)
+        nameSeparatorView.topAnchor.constraint(equalTo: nameTextField.bottomAnchor).isActive = true
+        nameSeparatorView.leftAnchor.constraint(equalTo: containerView.leftAnchor).isActive = true
+        nameSeparatorView.widthAnchor.constraint(equalTo: containerView.widthAnchor).isActive = true
+        nameSeparatorView.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        
+        containerView.addSubview(emailTextField)
+        emailTextField.topAnchor.constraint(equalTo: nameTextField.bottomAnchor).isActive = true
+        emailTextField.leftAnchor.constraint(equalTo: containerView.leftAnchor, constant: 12).isActive = true
+        emailTextField.widthAnchor.constraint(equalTo: containerView.widthAnchor).isActive = true
+        emailTextField.heightAnchor.constraint(equalTo: containerView.heightAnchor, multiplier: 1/3).isActive = true
+        
+        containerView.addSubview(emailSeparatorView)
+        emailSeparatorView.topAnchor.constraint(equalTo: emailTextField.bottomAnchor).isActive = true
+        emailSeparatorView.leftAnchor.constraint(equalTo: containerView.leftAnchor).isActive = true
+        emailSeparatorView.widthAnchor.constraint(equalTo: containerView.widthAnchor).isActive = true
+        emailSeparatorView.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        
+        containerView.addSubview(passwordTextField)
+        passwordTextField.topAnchor.constraint(equalTo: emailTextField.bottomAnchor).isActive = true
+        passwordTextField.leftAnchor.constraint(equalTo: containerView.leftAnchor, constant: 12).isActive = true
+        passwordTextField.widthAnchor.constraint(equalTo: containerView.widthAnchor).isActive = true
+        passwordTextField.heightAnchor.constraint(equalTo: containerView.heightAnchor, multiplier: 1/3).isActive = true
+        
+        containerView.addSubview(passwordSeparatorView)
+        passwordSeparatorView.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor).isActive = true
+        passwordSeparatorView.leftAnchor.constraint(equalTo: containerView.leftAnchor).isActive = true
+        passwordSeparatorView.widthAnchor.constraint(equalTo: containerView.widthAnchor).isActive = true
+        passwordSeparatorView.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        
     }
     
     func setupLoginButton() {
